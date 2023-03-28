d26674b772925420948a2d4fa07dd871c0bd2c4c
Allen <allengnr@gmail.com>
2023-03-28 21:29:49 +0800

    Fix object name is None

--- src/io/obj2.rs
+++ src/io/obj2.rs
@@ -23,7 +23,7 @@ pub fn deserialize_obj(raw_assets: &mut RawAssets, path: &PathBuf) -> Result<Sce
     }
 
     nodes.push(Node {
-        name: obj_data.name.unwrap(),
+        name: obj_data.name.unwrap_or("default object"),
         geometry: Some(Geometry::Triangles(TriMesh {
             positions: Positions::F32(positions),
             normals: Some(normals),
