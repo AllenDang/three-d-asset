ea51c05c15f4f55bbbc18a5a3f06c89b98443c78
Allen <allengnr@gmail.com>
2023-04-03 15:18:35 +0800

    Fix material index

--- src/io/obj2.rs
+++ src/io/obj2.rs
@@ -7,8 +7,8 @@ use std::{
 use tobj::LoadOptions;
 
 use crate::{
-    Color, Geometry, GeometryFunction, Indices, LightingModel, Node, NormalDistributionFunction,
-    PbrMaterial, Positions, Result, Scene, Texture2D, TriMesh, Vec2, Vec3,
+    Color, Geometry, Indices, LightingModel, Node, PbrMaterial, Positions, Result, Scene,
+    Texture2D, TriMesh, Vec2, Vec3,
 };
 
 use super::RawAssets;
@@ -24,6 +24,77 @@ pub fn deserialize_obj(raw_assets: &mut RawAssets, path: &PathBuf) -> Result<Sce
     )?;
 
     let p = path.parent().unwrap_or(Path::new(""));
+
+    let mut insert_tex = move |texture_path: String| {
+        if texture_path.is_empty() {
+            return;
+        }
+
+        let tex_path_part: Vec<&str> = texture_path
+            .split(['/', '\\'])
+            .filter(|p| !p.is_empty())
+            .collect();
+
+        let tex_path = p.join(&PathBuf::from_iter(tex_path_part.iter()));
+
+        let tex_bytes = std::fs::read(tex_path).unwrap();
+        raw_assets.insert(texture_path.clone(), tex_bytes);
+    };
+
+    let mut load_tex = move |texture_path: String| -> Option<Texture2D> {
+        let tex_de = raw_assets.deserialize(texture_path);
+        if let Ok(tex) = tex_de {
+            Some(tex)
+        } else {
+            None
+        }
+    };
+
+    let mut materials = Vec::new();
+    if let Ok(mats) = materials_data {
+        for m in mats.iter() {
+            if !m.diffuse_texture.is_empty() {
+                insert_tex(m.diffuse_texture);
+            }
+
+            if !m.ambient_texture.is_empty() {
+                insert_tex(m.ambient_texture);
+            }
+
+            if !m.normal_texture.is_empty() {
+                insert_tex(m.normal_texture);
+            }
+
+            if !m.specular_texture.is_empty() {
+                insert_tex(m.specular_texture);
+            }
+
+            if !m.dissolve_texture.is_empty() {
+                insert_tex(m.dissolve_texture);
+            }
+
+            if !m.shininess_texture.is_empty() {
+                insert_tex(m.shininess_texture);
+            }
+
+            materials.push(PbrMaterial {
+                name: m.name.clone(),
+                albedo: Color::from_rgba_slice(&[
+                    m.diffuse[0],
+                    m.diffuse[1],
+                    m.diffuse[2],
+                    m.dissolve,
+                ]),
+                albedo_texture: load_tex(m.diffuse_texture.clone()),
+                metallic: (m.specular[0] + m.specular[1] + m.specular[2]) / 3.0,
+                roughness: m.shininess,
+                metallic_roughness_texture: load_tex(m.specular_texture.clone()),
+                normal_texture: load_tex(m.normal_texture.clone()),
+                lighting_model: LightingModel::Blinn,
+                ..Default::default()
+            });
+        }
+    }
 
     let mut nodes = Vec::new();
 
@@ -60,67 +131,12 @@ pub fn deserialize_obj(raw_assets: &mut RawAssets, path: &PathBuf) -> Result<Sce
                 uvs: Some(uvs),
                 ..Default::default()
             })),
-            material_index: model.mesh.material_id,
+            material_index: materials
+                .iter()
+                .position(|m| &m.name == model.name.clone())
+                .unwrap(),
             ..Default::default()
         });
-    }
-
-    let mut load_tex = move |texture_path: String| -> Option<Texture2D> {
-        if !texture_path.is_empty() {
-            let tex_path_part: Vec<&str> = texture_path
-                .split(['/', '\\'])
-                .filter(|p| !p.is_empty())
-                .collect();
-
-            let tex_path = p.join(&PathBuf::from_iter(tex_path_part.iter()));
-
-            let tex_bytes = std::fs::read(tex_path).unwrap();
-            raw_assets.insert(texture_path.clone(), tex_bytes);
-
-            let tex_de = raw_assets.deserialize(texture_path);
-            if let Ok(tex) = tex_de {
-                Some(tex)
-            } else {
-                None
-            }
-        } else {
-            None
-        }
-    };
-
-    let mut materials = Vec::new();
-    if let Ok(mats) = materials_data {
-        for m in mats.iter() {
-            // let color = if m.diffuse[0] != m.diffuse[1] || m.diffuse[1] != m.diffuse[2] {
-            //     m.diffuse
-            // } else if m.specular[0] != m.specular[1] || m.specular[1] != m.specular[2] {
-            //     m.specular
-            // } else if m.ambient[0] != m.ambient[1] || m.ambient[1] != m.ambient[2] {
-            //     m.ambient
-            // } else {
-            //     m.diffuse
-            // };
-
-            materials.push(PbrMaterial {
-                name: m.name.clone(),
-                albedo: Color::from_rgba_slice(&[
-                    m.diffuse[0],
-                    m.diffuse[1],
-                    m.diffuse[2],
-                    m.dissolve,
-                ]),
-                albedo_texture: load_tex(m.diffuse_texture.clone()),
-                metallic: (m.specular[0] + m.specular[1] + m.specular[2]) / 3.0,
-                roughness: m.shininess,
-                metallic_roughness_texture: load_tex(m.specular_texture.clone()),
-                normal_texture: load_tex(m.normal_texture.clone()),
-                lighting_model: LightingModel::Cook(
-                    NormalDistributionFunction::TrowbridgeReitzGGX,
-                    GeometryFunction::SmithSchlickGGX,
-                ),
-                ..Default::default()
-            });
-        }
     }
 
     Ok(Scene {
